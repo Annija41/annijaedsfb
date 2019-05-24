0f955615d6ebab0fe149cc1876e037bfed9e07db
Annija Smilte <Annija.smilte41@gmail.com>
2019-05-23 23:51:11 -0700

    otra versja

---
+++ mainJS.js
@@ -0,0 +1,29 @@
+//1.Nodevinēt mainīgos kurš iegūst ienākumu saraksta elemetu no html dokumenta
+//2.Nodevinēt mainīgos kurš iegūst izdevumu saraksta elemetu no html dokumenta
+//3.Nodevinēt mainīgos kurš iegūst pieejamā budžeta vertības elemetu no html dokumenta
+
+//4.Izveidot funkciju_1, kas izmaina pieejamā budžeta vertību
+
+//5.Izveidot funkciju_2, kuru izsaucot, tiek pievienoti ienākumi vai izdevumi html elementu sarakstiem
+
+//6.Izveidot funkciju_3, kas izsaucas pēc pogas "pievienot" nospiešanas
+//7.Funkcijai_3 jāpārbauda vai apraksta un summas lauks ir aizpildīts, ja nav, tad izvada paziņojumu par to, ka kāds no laukiem nav aizpildīts
+//8.Funkcijai_3 ir jāizsauc funkcija_2, kas pievieno ienākumu vai izdevumu sarakstam jaunu ierakstu.
+
+//9.Lai pārbaudītu vai izveidotās funkcijas darbojas pareizi, izsauc tās zemāk un ievieto fake datus
+//10.Izsaukt funkciju_2, kas pievieno ienākumu sarakstam vertības
+//11.Izsaukt funkciju_2, kas pievieno izdevumu sarakstam vertības
+//12.Izsaukt funkciju_1, kas izmaina pieejamā budžeta vertību
+var y=document.getElementByClass("income left list");
+var x= document.getElementByClass("expenses right list");
+var t= document.getElementByClass("budget__title");
+var k =  document.getElementByClass("budget__total");
+var v = document.getElementByClass("budget__add");
+var l = document.getElementById("budget__value");
+
+function funkcija1 (value){
+    const funkcija1 = (value) =>
+    console.log(value)
+}
+ 
+
